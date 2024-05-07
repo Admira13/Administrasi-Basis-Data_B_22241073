ae7104dd229e51e1fe416ea5683c416b58063446
Admira13 <146043911+Admira13@users.noreply.github.com>
2024-05-07 17:38:24 +0800

    Add files via upload
    Selasa, 7 mei 2024

---
+++ Modul 1.sql
@@ -0,0 +1,8 @@
+-- NAMA	: ADMIRA SABTATIL YADAINI
+-- NIM	: 22241073
+-- MODUL 1
+
+-- Menggunakan database undikma_mart
+use undikma_mart;
+-- Mengambil data nama_produk dari tabel ms_produk_dqlab
+SELECT nama_produk FROM ms_produk_dqlab;
